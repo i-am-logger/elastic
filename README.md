d9a2c7b7ebd23b552178bb49be450436223fa47a
Ido Samuelson <ido.samuelson@gmail.com>
2013-12-05 13:24:14 -0500

    elastic search 0.90.7

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 elastic
 =======
+
+A Chocolatey package for Elastic Search.
+
+Elastic Search V0.90.7
+
+This package will install Elastic Search as a service as "automatic" and start it. 
+
+You can setup Elastic Search Enviorment Variables before installing this package. 
+
+Enjoy!
+
+
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+Install-ChocolateyZipPackage 'elastics' 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$path = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-0.90.7\bin"
+
+Install-ChocolateyPath $path
+
+$java_home = [Environment]::GetEnvironmentVariable("JAVA_HOME", "Machine")
+[Environment]::SetEnvironmentVariable("JAVA_HOME", $java_home)
+
+[Environment]::SetEnvironmentVariable("ES_START_TYPE", "auto")
+[Environment]::SetEnvironmentVariable("ES_START_TYPE", "auto")
+[Environment]::SetEnvironmentVariable("ELASTIC_SEARCH_PATH", "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-0.90.7\", "Machine")
+
+#if ([Environment]::GetEnvironmentVariable("DATA_DIR") -eq $null ) { throw "DATA_DIR Must be set" }
+#if ([Environment]::GetEnvironmentVariable("LOG_DIR") -eq $null ) { throw "LOG_DIR Must be set" }
+#if ([Environment]::GetEnvironmentVariable("CONF_DIR") -eq $null ) { throw "CONF_DIR Must be set" }
+#if ([Environment]::GetEnvironmentVariable("CONF_FILE") -eq $null ) { throw "CONF_FILE Must be set" }
+
+&"$path\service.bat" "install"
+
+&"$path\service.bat" "start"
